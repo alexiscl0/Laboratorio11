43ead806e32b8e76e589c5ffddea2df8d6ef1664
Alexis <acalcinala@unsa.edu.pe>
2025-10-25 13:11:27 -0500

    Ejercicio 5 js completado

--- javascript/cajero.js
+++ javascript/cajero.js
@@ -3,23 +3,23 @@ console.log("Monto a retirar ingresado: "+monto)
 let operac,contador
 while(true){
     if(monto>=100){
-        contador=Math.round(monto/100);
-        operac=monto%100;
+        contador=Math.floor(monto/100);
+        monto=monto%100;
         console.log("Hay "+contador+" billetes de 100");
     }
     else if(monto>=50){
-        contador=monto%50;
-        operac=monto%50
+        contador=Math.floor(monto/50);
+        monto=monto%50
         console.log("Hay "+contador+" billetes de 50")
     }
     else if(monto>=20){
-        contador=mont/20
-        operac=monto%20
+        contador=Math.floor(monto/20);
+        monto=monto%20
         console.log("Hay "+contador+" billetes de 20")
     }
     else if(monto>=10){
-        contador=monto%10
-        operac=monto%
+        contador=Math.floor(monto/10);
+        monto=monto%
         console.log("Hay "+contador+" billetes de 10")
     }
     else{
